66ac5f8a6bd32cb95a29950a656254e9a39fbd52
Radek VAIS <vaisr@students.zcu.cz>
2016-10-10 07:46:31 +0200

    add netservice member

---
+++ semestralka/MilostnyDopisServer/netservice/netservice.cpp
@@ -0,0 +1,13 @@
+#include "netservice.h"
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+
+Netservice::Netservice()
+{
+
+}
---
+++ semestralka/MilostnyDopisServer/netservice/netservice.h
@@ -0,0 +1,14 @@
+#ifndef NETSERVICE_H
+#define NETSERVICE_H
+
+
+class Netservice
+{
+    int port_number;
+    int server_socket;
+    int run_flag;
+public:
+    Netservice();
+};
+
+#endif // NETSERVICE_H
